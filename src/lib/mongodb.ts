9119dbe3f2f00e1a80f5e83cfc3f3d9f49985e80
Mohd Hamza <mohdhamza.me@gmail.com>
2024-10-05 12:01:01 +0530

    debugging

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -4,16 +4,16 @@ import mongoose from 'mongoose';
 
 export const connectMongoDB = async () => {
     const mongoDBuri = process.env.NEXT_PUBLIC_MONGODB_URI;
-
-    if (!mongoDBuri) {
-        throw new Error('Please define the NEXT_PUBLIC_MONGODB_URI environment variable inside .env.local');
-    }
-
-    if (mongoose.connection.readyState >= 1) {
-        return;
-    }
-
     try {
+        if (!mongoDBuri) {
+            throw new Error('Please define the NEXT_PUBLIC_MONGODB_URI environment variable inside .env.local');
+        }
+
+        if (mongoose.connection.readyState >= 1) {
+            return;
+        }
+
+
         await mongoose.connect(mongoDBuri);
         console.log("MongoDB connected");
     } catch (error) {
