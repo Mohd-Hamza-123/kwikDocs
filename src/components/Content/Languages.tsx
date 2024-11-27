417baf7ae02df609406e0165029ec3723db65a17
Mohd Hamza <mohdhamza.me@gmail.com>
2024-11-27 23:55:00 +0530

    working on Home page

--- src/components/Content/Languages.tsx
+++ src/components/Content/Languages.tsx
@@ -1,4 +1,3 @@
-import Image from 'next/image'
 import React from 'react'
 import { Button } from '../ui/button'
 export interface I_Language {
@@ -13,11 +12,12 @@ const Languages = ({
     title,
     description
 }: I_Language) => {
-    const isEven = _id % 2 === 0
+
+    const isEven = _id % 2 === 0;
+
     return (
-        <div className={`p-3 rounded-lg bg-white shadow-md dark:bg-surface-dark dark:text-white text-surface w-[70%] mx-auto my-8 flex ${isEven ? 'flex-row-reverse' :
-            ''}`}>
-            <div className="relative overflow-hidden bg-cover bg-no-repeat w-[40%]">
+        <div className={`p-3 rounded-lg bg-white shadow-md dark:bg-surface-dark dark:text-white text-surface w-[94%] lg:w-[70%] mx-auto my-8 flex flex-col lg:flex-row ${isEven ? 'flex-row-reverse' : ''}`}>
+            <div className="relative overflow-hidden bg-cover bg-no-repeat lg:w-[40%] w-full">
                 <img
                     className={`${isEven ? "rounded-r-lg" : 'rounded-l-lg'} w-full h-full object-contain object-center`}
                     src={image}
@@ -26,7 +26,7 @@ const Languages = ({
                     width={200}
                 />
             </div>
-            <div className="p-6 flex justify-around flex-col w-[60%]">
+            <div className="p-6 flex justify-around flex-col lg:w-[60%] w-full">
                 <h3 className="mb-2 text-4xl font-medium leading-tight text-center">
                     {title}
                 </h3>
@@ -36,7 +36,6 @@ const Languages = ({
                 <Button variant={'outline'}>Learn {title}</Button>
             </div>
         </div>
-
     )
 }
 
