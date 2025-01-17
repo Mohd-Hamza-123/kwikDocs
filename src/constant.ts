359162f0593286721b71adf5e709fc8d48cf35c5
Mohd Hamza <mohdhamza.me@gmail.com>
2025-01-17 21:41:27 +0530

    Adding Data Structure & Algorithm Documentation

--- src/constant.ts
+++ src/constant.ts
@@ -2,4 +2,4 @@ export const appName = "DocsNode"
 export const saltRounds = 10
 export const type_Reset_Email = 'RESET'
 export const type_Verify_Email = 'VERIFY'
-export const technologyEnums = ['coding language', 'framework', 'library', 'none', 'query language']
+export const technologyEnums = ['coding language', 'framework', 'library', 'DSA', 'none', 'query language']
