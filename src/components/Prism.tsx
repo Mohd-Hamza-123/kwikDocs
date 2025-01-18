a7bd196f2b5e76d3d3ca3c2443cb91899c5eb7ea
Mohd Hamza <mohdhamza.me@gmail.com>
2025-01-18 21:29:08 +0530

    Adding Data Structure & Algorithm Documentation

--- src/components/Prism.tsx
+++ src/components/Prism.tsx
@@ -11,25 +11,16 @@ import "prismjs/components/prism-tsx";
 import "prismjs/components/prism-typescript";
 import "prismjs/components/prism-python";
 import "prismjs/components/prism-java";
-import "prismjs/components/prism-dart";
 import "prismjs/components/prism-docker";
-import "prismjs/components/prism-ejs";
-import "prismjs/components/prism-git";
 import "prismjs/components/prism-django";
 import "prismjs/components/prism-sql";
 import "prismjs/components/prism-markdown";
 import "prismjs/components/prism-c";
 import "prismjs/components/prism-csharp";
 import "prismjs/components/prism-cpp"; 
-import "prismjs/components/prism-go";
-import "prismjs/components/prism-graphql";
-import "prismjs/components/prism-json";
-import "prismjs/components/prism-kotlin";
-import "prismjs/components/prism-mongodb";
-import "prismjs/components/prism-nginx";
-import "prismjs/components/prism-php";
-import "prismjs/components/prism-powershell";
-import "prismjs/components/prism-swift";
+
+
+
 
 import { useAppSelector } from "@/lib/hooks/hooks";
 import { docsInterface } from "@/models/docs.model";
@@ -38,7 +29,7 @@ const Prism = ({ children }: any) => {
   const doc: docsInterface | null = useAppSelector((state) => state.docs?.document);
 
   useEffect(() => {
-    prism.highlightAll();
+    prism?.highlightAll();
   }, [doc]);
   return <div>{children}</div>;
 };
