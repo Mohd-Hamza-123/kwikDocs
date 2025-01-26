84b0cdf3d7f0d26ca6ba893d0c5e0b5b57e9477b
Mohd Hamza <mohdhamza.me@gmail.com>
2025-01-26 20:03:25 +0530

    improving dark/light mode

--- src/context/TypicalContextProvider.tsx
+++ src/context/TypicalContextProvider.tsx
@@ -4,13 +4,18 @@ import { TypicalContextProvider } from './Typical-Context'
 
 const TypicalContextProviderWrapper = ({ children }: any) => {
     const [isSideBarOpen, setIsSideBarOpen] = useState(false);
+    const [theme, setTheme] = useState(localStorage.getItem("theme") || 'light');
+
+    const contextValue = {
+        isSideBarOpen,
+        setIsSideBarOpen,
+        theme,
+        setTheme,
+    };
+
     return (
         <TypicalContextProvider
-            value={{
-                isSideBarOpen,
-                setIsSideBarOpen,
-            }}
-        >
+            value={contextValue}>
             {children}
         </TypicalContextProvider>
     )
