e6824fa4b10a3cb6eac86a426730bf764a2145a9
Mohd Hamza <mohdhamza.me@gmail.com>
2025-02-08 16:03:17 +0530

    Added mdx functionality

--- src/components/Content/Languages.tsx
+++ src/components/Content/Languages.tsx
@@ -1,13 +1,13 @@
-'use client'
-import React from 'react'
-import Image from 'next/image';
-import { Button } from '../ui/button'
-import { LoadingPage } from '@/index';
-import { useRouter } from 'next/navigation';
-import { useQuery } from '@tanstack/react-query';
-import { useAppDispatch } from '@/lib/hooks/hooks';
-import { setDoc } from '@/lib/store/features/docsSlice';
-import { getAllTechnology } from '@/lib/API/techAPI/getAllTech';
+"use client";
+import React from "react";
+import Image from "next/image";
+import { Button } from "../ui/button";
+import { LoadingPage } from "@/index";
+import { useRouter } from "next/navigation";
+import { useQuery } from "@tanstack/react-query";
+import { useAppDispatch } from "@/lib/hooks/hooks";
+import { setDoc } from "@/lib/store/features/docsSlice";
+import { getAllTechnology } from "@/lib/API/techAPI/getAllTech";
 
 export interface I_Image {
     public_id: string;
@@ -18,75 +18,90 @@ export interface I_Language {
     name: string;
     image: I_Image;
     techType: string;
-    description: string
+    description: string;
 }
 
 const Technologies = () => {
-
     const router = useRouter();
     const dispatch = useAppDispatch();
 
     const {
         data: technology = [],
+        isError,
         error: technologyError,
         isPending: technologyPending,
         isSuccess: technologySuccess,
         isFetching: technologyFetching,
-        refetch
+        refetch,
     } = useQuery({
-        queryKey: ['technologies'],
+        queryKey: ["technologies"],
         queryFn: getAllTechnology,
         staleTime: Infinity,
     });
 
-    if (technologyPending) return <LoadingPage />
-
     const handleLearn = (techId: string) => {
         router.push(`/read-doc/${techId}`);
         dispatch(setDoc({ document: null }));
-    }
+    };
 
-    
+    //   console.log(technologyError?.response?.data);
 
-    return (
-        <div className='dark:bg-bgDark bg-gray-100'>
-            {technology?.map((techObj: any) => {
-                return <section
-                    key={techObj?.techType}>
-                    <h1 className='text-2xl lg:text-3xl text-center capitalize font-cursive'>{techObj?.techType}
-                    </h1>
-                    {techObj?.technologies?.map((tech: any, index: number) => (
-                        <div key={tech?._id + index} className={`dark:bg-containerDark rounded-lg bg-white shadow-md my-6 dark:text-white p-5 w-[94%] lg:w-[70%] mx-auto flex flex-col lg:flex-row ${(index + 1) % 2 === 0 ? 'lg:flex-row-reverse' : ''}`}>
-                            <div className="lg:w-[40%] w-full flex justify-center items-center">
-                                <Image
-                                    className={`rounded-md object-contain h-full w-full object-center`}
-                                    src={tech?.image?.secure_url}
-                                    alt={`${tech?.name} Image`}
-                                    height={200}
-                                    width={200}
-                                    quality={100}
-                                    priority={true}
-                                />
-                            </div>
-                            <div className="flex justify-around flex-col lg:w-[60%] w-full p-3">
-                                <h2 className="mb-2 text-2xl lg:text-3xl font-medium leading-tight text-center">
-                                    {tech?.name}
-                                </h2>
-                                <p className="mb-4 text-base text-gray-500 text-justify">
-                                    {tech?.description}
-                                </p>
-                                <Button
-                                    variant={'outline'}
-                                    onClick={() => handleLearn(tech?._id)}
-                                >Learn {tech?.name}
-                                </Button>
-                            </div>
-                        </div>
-                    ))}
-                </section>
-            })}
-        </div>
-    )
-}
+    if (technologyPending) return <LoadingPage />;
 
-export default Technologies
+    if (technologySuccess)
+        return (
+            <div className="dark:bg-bgDark bg-gray-100">
+                {technology?.map((techObj: any) => {
+                    return (
+                        <section key={techObj?.techType}>
+                            <h1 className="text-2xl lg:text-3xl text-center capitalize font-cursive">
+                                {techObj?.techType}
+                            </h1>
+                            {techObj?.technologies?.map((tech: any, index: number) => (
+                                <div
+                                    key={tech?._id + index}
+                                    className={`dark:bg-containerDark rounded-lg bg-white shadow-md my-6 dark:text-white p-5 w-[94%] lg:w-[70%] mx-auto flex flex-col lg:flex-row ${(index + 1) % 2 === 0 ? "lg:flex-row-reverse" : ""
+                                        }`}
+                                >
+                                    <div className="lg:w-[40%] w-full flex justify-center items-center">
+                                        <Image
+                                            className={`rounded-md object-contain h-full w-full object-center`}
+                                            src={tech?.image?.secure_url}
+                                            alt={`${tech?.name} Image`}
+                                            height={200}
+                                            width={200}
+                                            quality={100}
+                                            priority={true}
+                                        />
+                                    </div>
+                                    <div className="flex justify-around flex-col lg:w-[60%] w-full p-3">
+                                        <h2 className="mb-2 text-2xl lg:text-3xl font-medium leading-tight text-center">
+                                            {tech?.name}
+                                        </h2>
+                                        <p className="mb-4 text-base text-gray-500 text-justify">
+                                            {tech?.description}
+                                        </p>
+                                        <Button
+                                            variant={"outline"}
+                                            onClick={() => handleLearn(tech?._id)}
+                                        >
+                                            Learn {tech?.name}
+                                        </Button>
+                                    </div>
+                                </div>
+                            ))}
+                        </section>
+                    );
+                })}
+            </div>
+        );
+
+    if (isError)
+        return (
+            <h1 className="text-center capitalize font-bold mt-3">
+                {technologyError?.response?.data?.message}
+            </h1>
+        );
+};
+
+export default Technologies;
--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -1,5 +1,5 @@
 type iconsProps = React.HTMLAttributes<SVGElement>
-import { MdOutlineNightlightRound } from "react-icons/md";
+
 export const svgIcons = {
     logo: (props: iconsProps) => (
         <svg
@@ -26,33 +26,34 @@ export const svgIcons = {
     ),
     search: (props: iconsProps) => (
         <svg {...props} viewBox="0 0 24 24" fill="none">
-            <path d="M21 21L15 15M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" />
+            <path d="M21 21L15 15M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z" stroke="currentColor" stroke-width={2} strokeLinecap="round" strokeLinejoin="round" />
         </svg>
     ),
     home: (props: iconsProps) => (
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" {...props}>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" {...props}>
             <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
         </svg>
     ),
     createDoc: (props: iconsProps) => (
-        <svg {...props} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" >
+        <svg {...props} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" >
             <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5V4.875a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v14.25a2.25 2.25 0 002.25 2.25h6.75" />
             <path strokeLinecap="round" strokeLinejoin="round" d="M16.5 3v5.25a1.5 1.5 0 001.5 1.5h5.25M16.5 14.25v6M13.5 17.25h6" />
         </svg>
     ),
     setting: (props: iconsProps) => (
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" {...props}>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" {...props}>
             <path strokeLinecap="round" strokeLinejoin="round" d="M10.343 3.94c.09-.542.56-.94 1.11-.94h1.093c.55 0 1.02.398 1.11.94l.149.894c.07.424.384.764.78.93.398.164.855.142 1.205-.108l.737-.527a1.125 1.125 0 011.45.12l.773.774c.39.389.44 1.002.12 1.45l-.527.737c-.25.35-.272.806-.107 1.204.165.397.505.71.93.78l.893.15c.543.09.94.56.94 1.109v1.094c0 .55-.397 1.02-.94 1.11l-.893.149c-.425.07-.765.383-.93.78-.165.398-.143.854.107 1.204l.527.738c.32.447.269 1.06-.12 1.45l-.774.773a1.125 1.125 0 01-1.449.12l-.738-.527c-.35-.25-.806-.272-1.203-.107-.397.165-.71.505-.781.929l-.149.894c-.09.542-.56.94-1.11.94h-1.094c-.55 0-1.019-.398-1.11-.94l-.148-.894c-.071-.424-.384-.764-.781-.93-.398-.164-.854-.142-1.204.108l-.738.527c-.447.32-1.06.269-1.45-.12l-.773-.774a1.125 1.125 0 01-.12-1.45l.527-.737c.25-.35.273-.806.108-1.204-.165-.397-.505-.71-.93-.78l-.894-.15c-.542-.09-.94-.56-.94-1.109v-1.094c0-.55.398-1.02.94-1.11l.894-.149c.424-.07.765-.383.93-.78.165-.398.143-.854-.107-1.204l-.527-.738a1.125 1.125 0 01.12-1.45l.773-.773a1.125 1.125 0 011.45-.12l.737.527c.35.25.807.272 1.204.107.397-.165.71-.505.78-.929l.15-.894z" />
             <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
         </svg>
     ),
     dark: (props: iconsProps) => (
-        <svg {...props} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" className="w-6 h-6">
+        <svg {...props} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="{2}" strokeLinecap="round" strokeLinejoin="round" className="w-6 h-6">
             <path d="M21.05 12.22A9.84 9.84 0 0 1 12 22a10 10 0 1 1 6.92-17.33 7 7 0 0 0 2.13 7.55Z" />
         </svg>
+
     ),
     light: (props: iconsProps) => (
-        <svg {...props} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" >
+        <svg {...props} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" >
             <circle cx="12" cy="12" r="5" />
             <line x1="12" y1="1" x2="12" y2="3" />
             <line x1="12" y1="21" x2="12" y2="23" />
