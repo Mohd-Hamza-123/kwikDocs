37f93c68b01f9efa694f219a2d8fb40a6dd17c05
Mohd Hamza <mohdhamza.me@gmail.com>
2025-02-15 12:15:41 +0530

    making responsive

--- src/app/docs/[category]/page.tsx
+++ src/app/docs/[category]/page.tsx
@@ -9,12 +9,15 @@ import PostItems from '@/components/PostItems'
 import { MdOutlineZoomOutMap } from "react-icons/md";
 import { MDXContent } from '@/components/mdx-component';
 import { PostItemsProps } from '@/components/PostItems';
+import { useResponsiveContext } from "@/context/CSS-Context";
 
 const DocPage = ({ params }: { params: { category: string } }) => {
 
   const { category } = params
   const [post, setPost] = useState<PostItemsProps | null>(null)
   const [activeSlug, setActiveSlug] = useState('')
+
+  const { isDocIndexOpen, setIsDocIndexOpen } = useResponsiveContext();
 
   const displayPosts = posts?.filter((post) => post?.slug.indexOf(category) === 0 && post?.published);
 
@@ -23,10 +26,11 @@ const DocPage = ({ params }: { params: { category: string } }) => {
     setActiveSlug(post?.slug)
   }
 
+
   return (
-    <main className="flex flex-col-reverse lg:flex-row relative h-[90vh] overflow-x-hidden w-full justify-between">
+    <div className="flex flex-col-reverse lg:flex-row relative h-[90vh] overflow-x-hidden w-full justify-between">
       {/* Sidebar */}
-      <section className="w-[100%] lg:w-[20%] border border-r-3 max-h-[88vh] overflow-y-scroll absolute lg:sticky top-0 bg-slate-50 dark:bg-bgDark z-20 py-2 dark:border-gray-700 lg:block">
+      <section className={`w-[100%] lg:w-[20%] border border-r-3 max-h-[90vh] overflow-y-scroll absolute lg:sticky top-0 bg-slate-50 dark:bg-bgDark z-20 py-2 dark:border-gray-700 lg:block ${isDocIndexOpen ? "block" : "hidden"} lg:block`}>
         <p className="mt-4 text-center font-semibold capitalize">{category}</p>
 
         <ul className="flex flex-col gap-1 mt-5">
@@ -47,23 +51,35 @@ const DocPage = ({ params }: { params: { category: string } }) => {
 
       </section>
 
-      {/* Main Content */}
-      {post && <article className="container py-6 prose dark:prose-invert max-w-3xl mx-auto">
-        <h1 className="mb-2">{post.title}</h1>
 
-        {post.description ? (
-          <p className="text-xl mt-0 text-muted-foreground">{post.description}</p>
-        ) : null}
-        <hr className="my-4" />
-        <MDXContent code={post.body} />
-      </article>}
+      <section className="w-full lg:w-[60%] overflow-y-scroll">
 
+        {post && <article className="container py-4 prose dark:prose-invert max-w-3xl mx-auto">
+          <div className="flex justify-between items-center">
+            <div>
+              <h1 className="mb-2">{post.title}</h1>
+              {post.description ? (
+                <p className="text-xl mt-0 text-muted-foreground">{post.description}</p>
+              ) : null}
+            </div>
 
-      <section className="lg:w-[20%] border">
+            <Link
+              className="hidden lg:block"
+              href={`/document/${post?.slugAsParams}`}><MdOutlineZoomOutMap className="h-5 w-5" /></Link>
+
+          </div>
+
+          <hr className="my-4" />
+          <MDXContent code={post.body} />
+        </article>}
+
+      </section>
+
+      <section className="w-full lg:w-[20%] border">
 
 
       </section>
-    </main>
+    </div>
   )
 }
 
--- src/components/PostItems.tsx
+++ src/components/PostItems.tsx
@@ -1,5 +1,8 @@
 'use client'
+import { useResponsiveContext } from '@/context/CSS-Context';
+import { useTypicalContext } from '@/context/Typical-Context';
 import React, { useState } from 'react'
+
 export interface PostItemsProps {
     slug: string;
     title: string;
@@ -9,16 +12,25 @@ export interface PostItemsProps {
     published: boolean
 }
 
-const PostItems = ({ post, category, renderPost, activeSlug }: {
-    post: PostItemsProps, category: string, renderPost: any, activeSlug: string
-}) => {
-    // const [selectedPostSlug, setSelectedPostSlug] = useState('')
-    // console.log(selectedPostSlug)
+const PostItems = ({
+    post,
+    category,
+    renderPost,
+    activeSlug }: {
+        post: PostItemsProps,
+        category: string,
+        renderPost: any,
+        activeSlug: string
+    }) => {
+    const {
+        isDocIndexOpen,
+        setIsDocIndexOpen
+    } = useResponsiveContext();
     return (
         <li
             onClick={() => {
                 renderPost(post)
-                // setSelectedPostSlug(post?.slug)
+                setIsDocIndexOpen(false)
             }}
             className={`capitalize text-sm font-bold rounded-sm px-2 py-3 md:py-2 sm:py-4 list-none cursor-pointer border-b border-gray-200 dark:text-gray-300 ${post?.slug === activeSlug
                 ? "bg-indigo-600 text-white"
