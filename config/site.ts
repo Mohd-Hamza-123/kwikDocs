3cab23c4d55e10f39e7febf5724ee02c8328a1d7
Mohd Hamza <mohdhamza.me@gmail.com>
2025-02-19 16:15:39 +0530

    fixing bug

--- config/site.ts
+++ config/site.ts
@@ -1,13 +1,14 @@
 import conf from "@/conf/conf"
 export const siteConfig = {
-    name: "Docs Node",
+    name: "Kwik Docs",
     url: conf.api_end_point,
     description: "Learn Simply: Your Beginner-Friendly Guide to Mastering Concepts",
     author: "Mohd Hamza",
     links: {
         twitter: '',
         instagram: 'https://www.instagram.com/byte_master_hamza/',
-        github: 'https://github.com/Mohd-Hamza-123'
+        github: 'https://github.com/Mohd-Hamza-123',
+        reddit : 'https://www.reddit.com/user/MathematicianOld479/'
     }
 }
 
