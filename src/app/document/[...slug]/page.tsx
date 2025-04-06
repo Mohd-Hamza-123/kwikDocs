69021fcde40b398e2565e1eaf8631d0fe03a9b31
Mohd Hamza <mohdhamza.me@gmail.com>
2025-04-06 16:40:49 +0530

    added django docs - django templates

--- src/app/document/[...slug]/page.tsx
+++ src/app/document/[...slug]/page.tsx
@@ -31,9 +31,9 @@ export default async function PostPage({ params }: PostPageProps) {
     return <article className="container py-6 prose dark:prose-invert max-w-[100dvw] lg:max-w-[75dvw] mx-auto">
         <h1 className="text-xl">{post.title}</h1>
         {post.description ? (
-            <p className="text-xl mt-0 text-muted-foreground">{post.description}</p>
+            <p className="text-md mt-0 text-muted-foreground">{post.description}</p>
         ) : null}
-        <hr className="my-4" />
+        <hr className="my-3" />
         <MDXContent code={post.body} />
     </article>
 }
