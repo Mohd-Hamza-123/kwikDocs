d20e41365b8f02c08e64f6414981d7c60937c4c1
Mohd Hamza <mohdhamza.me@gmail.com>
2025-06-27 19:32:52 +0530

    fix : bug fix

--- src/app/InitializationWrapper.tsx
+++ src/app/InitializationWrapper.tsx
@@ -1,11 +1,12 @@
 'use client'
 
-import React, { ReactNode } from 'react'
+import React, { ReactNode, useEffect } from 'react'
 import logoutAPI from '@/lib/API/Auth/logout'
 import { useQuery } from '@tanstack/react-query'
 import getProfile from '@/lib/API/Auth/profile'
 import { useAppDispatch } from '@/lib/hooks/hooks'
 import { login, logout } from '@/lib/store/features/authSlice'
+import { changeTheme } from '@/lib/store/features/editorSlice'
 
 const InitializationWrapper = ({ children }: { children: ReactNode }) => {
 
@@ -32,6 +33,10 @@ const InitializationWrapper = ({ children }: { children: ReactNode }) => {
             .then(() => dispatch(logout()))
     }
 
+    useEffect(() => {
+        const EditorTheme = localStorage.getItem("editorTheme") || 'monokai'
+        dispatch(changeTheme({ theme: EditorTheme }))
+    }, [])
 
     return (<>{children}</>)
 }
--- src/lib/store/features/editorSlice.ts
+++ src/lib/store/features/editorSlice.ts
@@ -4,22 +4,18 @@ import { createSlice } from '@reduxjs/toolkit'
 export type ThemeKeys = keyof typeof Themes;
 
 interface EditorState {
-    theme: ThemeKeys; 
+    theme: ThemeKeys;
 }
 
-const defaultTheme = localStorage.getItem("editorTheme") || 'monokai'
-
 const initialState: EditorState = {
-    theme: defaultTheme as ThemeKeys
+    theme: 'monokai'
 }
 
 export const editorSlice = createSlice({
     name: 'editor',
     initialState,
     reducers: {
-        changeTheme: (state, action) => {
-            state.theme = action.payload.theme
-        }
+        changeTheme: (state, action) => state.theme = action.payload.theme
     },
 })
 
