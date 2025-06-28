1df44a6dc11da78c5502ee0ff797c8e2a90c9b59
Mohd Hamza <mohdhamza.me@gmail.com>
2025-06-28 19:29:48 +0530

    initial commit from feature/folder branch

--- src/app/api/docs/route.ts
+++ src/app/api/docs/route.ts
@@ -1,14 +1,14 @@
 // pages/api/getFolders.ts
 import { NextApiRequest, NextApiResponse } from 'next';
 import fs from 'fs/promises';
-
-export default async function POST(req: NextApiRequest, res: NextApiResponse) {
+import { NextRequest, NextResponse } from "next/server";
+export default async function POST(req:NextRequest,res:NextResponse) {
   try {
     const items = await fs.readdir('./src/content', { withFileTypes: true });
     const folders = items.filter((item) => item.isDirectory()).map((item) => item.name);
-    res.status(200).json({ folders });
+    return NextResponse.json({success : true})
   } catch (err) {
     console.error('Error reading directory:', err);
-    res.status(500).json({ error: 'Failed to read directories' });
+     return NextResponse.json({success : true})
   }
 }
