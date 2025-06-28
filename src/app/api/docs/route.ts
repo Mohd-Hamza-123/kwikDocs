f96dcad31241986f8c557ddc368a1e4ece854ce9
Mohd Hamza <mohdhamza.me@gmail.com>
2025-06-28 19:31:56 +0530

    initial commit from feature/folder branch

--- src/app/api/docs/route.ts
+++ src/app/api/docs/route.ts
@@ -2,7 +2,7 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import fs from 'fs/promises';
 import { NextRequest, NextResponse } from "next/server";
-export default async function POST(req:NextRequest,res:NextResponse) {
+export async function POST(req:NextRequest,res:NextResponse) {
   try {
     const items = await fs.readdir('./src/content', { withFileTypes: true });
     const folders = items.filter((item) => item.isDirectory()).map((item) => item.name);
