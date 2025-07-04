88fb65936a9e8df5057535886b59d0fb351f0e25
Mohd Hamza <mohdhamza.me@gmail.com>
2025-07-04 16:57:12 +0530

    bug:fix bug

--- src/app/api/contentTree/[tech]/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from 'next/server'
-import path from 'path'
-import getContentTree from '@/services/helpers/getContentTree'
-
-export async function GET(request: Request, { params }: { params: { tech: string } }) {
-  const folderPath = path.join(process.cwd(), 'src', 'content', params.tech)
-  const tree = await getContentTree(folderPath)
-  return NextResponse.json(tree || [])
-}
