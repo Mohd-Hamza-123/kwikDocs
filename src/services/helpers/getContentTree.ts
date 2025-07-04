1a916339ab02277c928ef5f2f1e1dbcaabd19017
Mohd Hamza <mohdhamza.me@gmail.com>
2025-07-04 22:28:21 +0530

    bug : fix bug

--- src/services/helpers/getContentTree.ts
+++ src/services/helpers/getContentTree.ts
@@ -6,17 +6,18 @@ import path from "path";
 export type FileNode = {
     name: string;
     type: "file" | "directory";
+    slug?: string;
     children?: FileNode[];
 }
 
 const getContentTree = async (source: string): Promise<FileNode[] | null> => {
     try {
-
         const dirents = await fs.readdir(source, { withFileTypes: true })
 
         const structure = dirents?.map(async (dirent) => {
+            const subPath = path.join(source, dirent?.name)
+
             if (dirent.isDirectory()) {
-                const subPath = path.join(source, dirent?.name)
                 const children = await getContentTree(subPath)
                 return {
                     name: dirent?.name,
@@ -24,9 +25,16 @@ const getContentTree = async (source: string): Promise<FileNode[] | null> => {
                     children: children || []
                 }
             } else {
+
+                const filePath = path.join(dirent.parentPath, dirent?.name).replaceAll(/\\/g, "/")
+                const splittedPath = filePath.split('/')
+                const index = splittedPath.indexOf("content")
+                const slug = splittedPath.slice(index + 1, splittedPath.length).join("/")
+
                 return {
                     name: dirent?.name,
-                    type: "file" as const
+                    type: "file" as const,
+                    slug: slug.replaceAll(".mdx", "")
                 }
             }
         })
