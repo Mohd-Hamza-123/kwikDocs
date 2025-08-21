d67bca5b747d4f125b5c3e8feccfc13263eb90f3
Mohd Hamza <mohdhamza.me@gmail.com>
2025-08-21 10:29:26 +0530

    working on html-css code editor

--- src/app/api/content-tree/[tech]/route.ts
+++ src/app/api/content-tree/[tech]/route.ts
@@ -2,18 +2,19 @@ import { NextResponse } from 'next/server'
 import path from 'path'
 import getContentTree from '@/services/helpers/getContentTree'
 
+// Do NOT destructure directly in params – type it properly
 export async function GET(
-  request: Request,
-  context: { params: { tech: string } }
+    request: Request,
+    context: any // context type
 ) {
-  try {
-    const { tech } = context.params
-    const folderPath = path.join(process.cwd(), 'src', 'content', tech)
-    const tree = await getContentTree(folderPath)
+    try {
+        const tech = context?.params?.tech
+        const folderPath = path.join(process.cwd(), 'src', 'content', tech)
+        const tree = await getContentTree(folderPath)
 
-    return NextResponse.json({ tree: tree || null }, { status: 200 })
-  } catch (error: any) {
-    console.error(error?.message)
-    return NextResponse.json({ error: error?.message || null }, { status: 500 })
-  }
+        return NextResponse.json({ tree: tree || null }, { status: 200 })
+    } catch (error: any) {
+        console.error(error?.message)
+        return NextResponse.json({ error: error?.message || null }, { status: 500 })
+    }
 }
