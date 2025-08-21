102637e7b70ff3281e42a38d23331ae2b1c82be3
Mohd Hamza <mohdhamza.me@gmail.com>
2025-08-21 10:05:30 +0530

    working on html-css code editor

--- src/app/api/content-tree/[tech]/route.ts
+++ src/app/api/content-tree/[tech]/route.ts
@@ -2,13 +2,18 @@ import { NextResponse } from 'next/server'
 import path from 'path'
 import getContentTree from '@/services/helpers/getContentTree'
 
-export async function GET(request: Request, { params }: { params: { tech: string } }) {
-    try {
-        const folderPath = path.join(process.cwd(), 'src', 'content', params.tech)
-        const tree = await getContentTree(folderPath)
-        return NextResponse.json({ tree: tree || null }, { status: 200 })
-    } catch (error) {
+export async function GET(
+  request: Request,
+  context: { params: { tech: string } }
+) {
+  try {
+    const { tech } = context.params
+    const folderPath = path.join(process.cwd(), 'src', 'content', tech)
+    const tree = await getContentTree(folderPath)
 
-    }
-
+    return NextResponse.json({ tree: tree || null }, { status: 200 })
+  } catch (error: any) {
+    console.error(error?.message)
+    return NextResponse.json({ error: error?.message || null }, { status: 500 })
+  }
 }
