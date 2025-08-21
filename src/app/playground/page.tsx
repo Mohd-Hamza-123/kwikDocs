15a882b21f206c523b8d1e8cb7d46d567477e815
Mohd Hamza <mohdhamza.me@gmail.com>
2025-08-21 14:36:51 +0530

    working on html-css editor

--- src/app/playground/page.tsx
+++ src/app/playground/page.tsx
@@ -8,29 +8,29 @@ import {
 } from "@/components/ui/card";
 
 const languages = [
-    { name: "HTML & CSS", image: "language/html-css.png", path: "/editor/html-css" },
-    { name: "HTML, CSS & JavaScript", image: "/language/web.png", path: "/editor/web" },
-    { name: "Node JS", image: "/language/javascript.jpg", path: "/editor/nodejs" },
-    { name: "TypeScript", image: "/language/typescript.jpg", path: "/editor/typescript" },
-    { name: "Python", image: "/language/python.png", path: "/editor/python" },
-    { name: "Java", image: "/language/java.png", path: "/editor/java" },
-    { name: "C++", image: "/language/cpp.png", path: "/editor/cpp" },
-    { name: "C", image: "/language/c.png", path: "/editor/c" },
-    { name: "Markdown", image: "/language/markdown.png", path: "/editor/markdown" },
+    { name: "HTML & CSS", image: "language/html-css.png", path: "/playground-editor/html-css" },
+    // { name: "HTML, CSS & JavaScript", image: "/language/web.png", path: "/playground-editor/web" },
+    // { name: "Node JS", image: "/language/javascript.jpg", path: "/playground-editor/nodejs" },
+    // { name: "TypeScript", image: "/language/typescript.jpg", path: "/playground-editor/typescript" },
+    // { name: "Python", image: "/language/python.png", path: "/playground-editor/python" },
+    // { name: "Java", image: "/language/java.png", path: "/playground-editor/java" },
+    // { name: "C++", image: "/language/cpp.png", path: "/playground-editor/cpp" },
+    // { name: "C", image: "/language/c.png", path: "/playground-editor/c" },
+    // { name: "Markdown", image: "/language/markdown.png", path: "/playground-editor/markdown" },
 ];
 
 
 const Playground = () => {
     return (
-        <div className="min-h-screen p-8 bg-white dark:bg-bgDark">
+        <div className="min-h-[91dvh] w-full py-8 px-4 bg-white dark:bg-bgDark">
             <h1 className="text-center text-gray-900 dark:text-gray-100 text-3xl font-extrabold mb-10 tracking-wide">
                 Choose Your Language
             </h1>
-            <div className="max-w-6xl mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
+            <div className="w-full md:w-[90%] mx-auto flex justify-center gap-6 flex-wrap">
                 {languages.map((language) => (
-                    <Link href={language.path} key={language.name}>
+                    <Link href={language.path} key={language.name} className='w-full md:w-[30%]'>
                         <Card
-                            className="group relative overflow-hidden rounded-xl shadow-md border border-gray-200 dark:border-gray-700 transition-transform transform hover:scale-105 hover:shadow-lg bg-gradient-to-br from-white via-gray-100 to-gray-200 dark:from-[#1e1e1e] dark:via-[#121212] dark:to-[#2c2c2c]">
+                            className="w-full group relative overflow-hidden rounded-xl shadow-md border border-gray-200 dark:border-gray-700 transition-transform transform hover:scale-105 hover:shadow-lg bg-gradient-to-br from-white via-gray-100 to-gray-200 dark:from-[#1e1e1e] dark:via-[#121212] dark:to-[#2c2c2c]">
                             <CardContent className="p-6 flex flex-col items-center text-center">
                                 <Image
                                     height={100}
