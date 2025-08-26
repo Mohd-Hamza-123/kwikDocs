de90cf02604874e02a66ba6013beae522a9584b2
Mohd Hamza <mohdhamza.me@gmail.com>
2025-08-26 11:33:03 +0530

    feature : add html-css editor with monaco

--- src/app/playground-editor/html-css/page.tsx
+++ src/app/playground-editor/html-css/page.tsx
@@ -1,154 +1,36 @@
 'use client'
 import './page.css'
 import { toast } from '@/hooks/use-toast';
-import { css } from '@codemirror/lang-css';
-import { html } from '@codemirror/lang-html';
 import { svgIcons } from '@/components/icons';
 import { Button } from '@/components/ui/button';
-import { EditorState } from '@codemirror/state';
 import sanitizeHtml from '@/utils/Sanitize-html';
 import { useAppSelector } from '@/lib/hooks/hooks';
 import { CODE_SNIPPETS, Themes } from '@/constant';
-import copyToClipBoard from '@/utils/copyToClipboard';
 import injectCssIntoHtml from '@/utils/InjectCssIntoHtml';
-import createEmmetKeyMap from '@/components/Code-Editor/HTML/Emmet';
 import React, { useCallback, useEffect, useState, useRef } from 'react'
-import { EditorView, highlightActiveLine, keymap } from '@codemirror/view';
-import { createCssPropertyKeyMap } from '@/utils/codeMirror/automaticClose';
-import { autocompletion, closeBracketsKeymap, completionKeymap, closeBrackets } from '@codemirror/autocomplete';
-import CodeMirrorThemeDropdown from '@/components/Code-Editor/CodeMirrorThemeDropdown';
-import {
-    history,
-    defaultKeymap,
-    historyKeymap
-} from '@codemirror/commands';
+import HtmlMonacoEditor from '@/components/Code-Editor/Monaco/HtmlMonacoEditor';
+import CssMonacoEditor from '@/components/Code-Editor/Monaco/CssMonacoEditor';
 
 
 const Page = () => {
 
-    const isCodeEditable = true
-    const editor = useRef<HTMLDivElement>(null)
-    const cssEditor = useRef<HTMLDivElement>(null)
+    const htmlRef = useRef<any>(null)
+    const cssRef = useRef<any>(null)
     const outputRef = useRef<HTMLDivElement>(null)
-    const htmlViewRef = useRef<EditorView | null>(null)
     const [visible, setVisible] = useState<'html' | 'css' | 'output'>('html')
     const [outputToggle, setOutputToggle] = useState(false)
-    const [cssCode, setCssCode] = useState(CODE_SNIPPETS?.css)
-    const [htmlCode, setHtmlCode] = useState(CODE_SNIPPETS?.html)
-    const theme = useAppSelector((state) => state.editorSlice.theme)
-
-
-    const htmlState = () => {
-        if (!editor.current) return
-        const state = EditorState.create({
-            doc: htmlCode,
-            extensions: [
-                html(),
-                Themes[theme],
-                history(),
-                autocompletion(),
-                highlightActiveLine(),
-                keymap.of([
-                    ...defaultKeymap,
-                    ...historyKeymap,
-                    ...completionKeymap,
-                    ...closeBracketsKeymap,
-                    createEmmetKeyMap('html'),
-                ]),
-                EditorState.readOnly.of(!isCodeEditable),
-                EditorView.updateListener.of((update) => {
-                    const html = update.state.doc?.toString()
-                    setHtmlCode(html)
-                }),
-                EditorView.theme({
-                    "&": {
-                        height: "100%",
-                        width: "100%"
-                    },
-                }),
-            ]
-        })
-
-        const HtmlView = new EditorView({
-            state,
-            parent: editor.current,
-        });
-        htmlViewRef.current = HtmlView
-        return HtmlView
-    }
-    const cssState = () => {
-        if (!cssEditor.current) return
-
-        const cssState = EditorState.create({
-            doc: cssCode,
-            extensions: [
-                css(),
-                Themes[theme],
-                history(),
-                createCssPropertyKeyMap(),
-                closeBrackets(),
-                autocompletion(),
-                highlightActiveLine(),
-                keymap.of([
-                    ...defaultKeymap,
-                    ...historyKeymap,
-                    ...completionKeymap,
-                    ...closeBracketsKeymap,
-                    createEmmetKeyMap('css'),
-                ]),
-                EditorState.readOnly.of(!isCodeEditable),
-                EditorView.updateListener.of((update) => {
-                    // console.log(update)
-                    // console.log(update.docChanged)
-                    // console.log(update.state.doc?.toString()) // how ? 
-                    setCssCode(update.state.doc?.toString())
-                }),
-                EditorView.theme({
-                    "&": {
-                        height: "100%",
-                        width: "100%"
-                    },
-                }),
-            ]
-        })
-
-
-
-        const cssView = new EditorView({
-            state: cssState,
-            parent: cssEditor.current,
-        });
-
-        return cssView
-    }
-
-
-    useEffect(() => {
-
-        const htmlView = htmlState()
-        const cssView = cssState()
-
-        return () => {
-            if (htmlView) htmlView.destroy()
-            if (cssView) cssView.destroy()
-        }
-    }, [theme])
 
 
     const executeCode = () => {
         try {
+
+            const htmlCode = htmlRef.current?.getValue?.() ?? ''
+            const cssCode = cssRef.current?.getValue?.() ?? ''
+            console.log(htmlCode)
+            console.log(cssCode)
             setVisible('output')
             const sanitizedHtml = sanitizeHtml(htmlCode)
-            setHtmlCode(sanitizedHtml)
-            if (htmlViewRef.current) {
-                htmlViewRef.current.dispatch({
-                    changes: {
-                        from: 0,
-                        to: htmlViewRef.current.state.doc.length,
-                        insert: sanitizedHtml,
-                    }
-                })
-            }
+            // setHtmlCode(sanitizedHtml)
 
             const iframe = document.createElement('iframe')
             iframe.style.width = "100%"
@@ -162,62 +44,57 @@ const Page = () => {
                 setOutputToggle(true)
             }
 
-        } catch (error: any) {
-            console.log(error)
+        } catch (error: unknown) {
+            if (error instanceof Error) {
+                toast({
+                    title: 'Error',
+                    variant: 'destructive',
+                    description: error?.message || "Something went wrong",
+                })
+                console.log(error.message)
+            }
             setOutputToggle(false)
-            toast({
-                title: 'Error',
-                variant: 'destructive',
-                description: error?.message || "Something went wrong",
-            })
         }
     }
 
     return (
-        <div className='w-full flex flex-col lg:flex-row gap-4 p-4 poppins'>
+        <div className='w-full flex flex-col lg:flex-row gap-2 md:gap-4 p-2 md:p-4 poppins border h-[91dvh]'>
 
-            <div id='playground-editor-html-css-navigation' className="flex flex-row lg:flex-col justify-center items-center gap-5 p-2 rounded-xl shadow-md sticky top-0 bg-black/40 backdrop-blur-md border border-gray-700 overflow-x-scroll">
+            <div id='playground-editor-html-css-navigation' className="flex flex-row lg:flex-col justify-center items-center gap-8 p-2 rounded-xl shadow-md  bg-black/40 backdrop-blur-md border border-gray-700 overflow-x-scroll">
                 <Button
                     onClick={() => setVisible('html')}
-                    variant={visible === 'html' ? 'default' : 'secondary'}
-                    className="flex items-center gap-2">
-                    <svgIcons.html className="w-7 h-7" />
+                    className='md:h-10'
+                    variant={visible === 'html' ? 'default' : 'secondary'}>
+                    <svgIcons.html className="w-4 h-4 md:w-7 md:h-7" />
                 </Button>
                 <Button
                     variant={visible === 'css' ? 'default' : 'secondary'}
-                    className="flex items-center gap-2"
+                    className='md:h-10'
                     onClick={() => setVisible('css')}>
-                    <svgIcons.css className="w-7 h-7" />
+                    <svgIcons.css className="w-4 h-4 md:w-7 md:h-7" />
                 </Button>
                 <Button
                     variant={visible === 'output' ? 'outline' : 'secondary'}
                     onClick={executeCode}
-                    className="px-4 py-2 flex items-center gap-2 font-semibold bg-green-500 hover:bg-green-600 text-white rounded-lg shadow-lg">
-                    <svgIcons.play className="w-7 h-7" />
+                    className="px-4 py-2 font-semibold bg-green-500 hover:bg-green-600 text-white rounded-lg shadow-lg md:h-10">
+                    <svgIcons.play className="w-4 h-4 md:w-7 md:h-7" />
                 </Button>
-                <CodeMirrorThemeDropdown triggerType="button" />
             </div>
 
-            <section className='w-full'>
-                <div
-                    ref={editor}
-                    className={`w-full h-[80dvh] rounded-md shadow-md relative overflow-y-scroll ${visible === 'html' ? 'block' : 'hidden'}`}>
-                    <svgIcons.copy
-                        onClick={() => copyToClipBoard(htmlCode)}
-                        className="h-9 w-9 bg-gray-800 text-gray-300 hover:text-blue-400 hover:bg-gray-700 cursor-pointer p-2 rounded-full absolute top-3 right-3 shadow-md transition z-10"
-                    />
-                </div>
-                <div
-                    ref={cssEditor}
-                    className={`w-full h-[80dvh] rounded-md shadow-md relative overflow-y-scroll ${visible === 'css' ? 'block' : 'hidden'}`}>
-                    <svgIcons.copy
-                        onClick={() => copyToClipBoard(cssCode)}
-                        className="h-9 w-9 bg-gray-800 text-gray-300 hover:text-blue-400 hover:bg-gray-700 cursor-pointer p-2 rounded-full absolute top-3 right-3 shadow-md transition z-10"
-                    />
-                </div>
+            <section className='w-full h-[80dvh] lg:h-[86dvh] rounded-md overflow-hidden'>
+                <HtmlMonacoEditor
+                    visible={`${visible === 'html' ? 'block' : 'hidden'}`}
+                    className="w-full h-full rounded-md shadow-md relative overflow-y-scroll"
+                    ref={htmlRef}
+                />
+                <CssMonacoEditor
+                    visible={`${visible === 'css' ? 'block' : 'hidden'}`}
+                    className="w-full h-full rounded-md shadow-md relative overflow-y-scroll"
+                    ref={cssRef}
+                />
                 <div
                     ref={outputRef}
-                    className={`${outputToggle ? "block" : "hidden"} w-full border rounded-md h-[80dvh] overflow-auto bg-white ${visible === 'output' ? 'block' : 'hidden'}`}>
+                    className={`${outputToggle ? "block" : "hidden"} w-full border rounded-md h-full overflow-auto bg-white ${visible === 'output' ? 'block' : 'hidden'}`}>
                 </div>
             </section>
         </div>
--- src/constant.ts
+++ src/constant.ts
@@ -28,14 +28,6 @@ export const technologyEnums = [
   "Containerization Platform",
 ]
 
-export const LANGUAGE_VERSIONS = {
-  javascript: "18.15.0",
-  typescript: "5.0.3",
-  python: "3.10.0",
-  java: "15.0.2",
-  csharp: "6.12.0",
-  php: "8.2.3",
-};
 
 export const CODE_SNIPPETS = {
   css: `*{
@@ -43,6 +35,7 @@ export const CODE_SNIPPETS = {
   padding: 0;
   box-sizing: border-box;
   }
+
   html,body{
   width : 100%;
   height: 100%;
@@ -53,8 +46,6 @@ export const CODE_SNIPPETS = {
   typescript: `\ntype Params = {\n\tname: string;\n}\n\nfunction greet(data: Params) {\n\tconsole.log("Hello, " + data.name + "!");\n}\n\ngreet({ name: "Alex" });\n`,
   python: `\ndef greet(name):\n\tprint("Hello, " + name + "!")\n\ngreet("Alex")\n`,
   java: `\npublic class HelloWorld {\n\tpublic static void main(String[] args) {\n\t\tSystem.out.println("Hello World");\n\t}\n}\n`,
-  csharp:
-    'using System;\n\nnamespace HelloWorld\n{\n\tclass Hello { \n\t\tstatic void Main(string[] args) {\n\t\t\tConsole.WriteLine("Hello World in C#");\n\t\t}\n\t}\n}\n',
   html: `<html>
     <head>
         <meta charset="UTF-8">
