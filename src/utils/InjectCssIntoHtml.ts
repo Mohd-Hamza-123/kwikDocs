66294a2df0e834d3a370179274ad0bb548d45211
Mohd Hamza <mohdhamza.me@gmail.com>
2025-09-02 12:04:10 +0530

    ui : update home page ui

--- src/utils/InjectCssIntoHtml.ts
+++ src/utils/InjectCssIntoHtml.ts
@@ -265,14 +265,13 @@ export function customConfirm(message: string): Promise<boolean> {
   });
 }
 
-
 import { CSP } from "@/constant";
 
 
 export default function injectCssAndJavascriptIntoHtml(
   html: string,
   css: string,
-  javascript: string
+  javascript?: string
 ) {
   const headContentRegex = /<head\b[^>]*>([\s\S]*?)<\/head>/i;
   let headContent = html.match(headContentRegex);
@@ -349,3 +348,22 @@ export default function injectCssAndJavascriptIntoHtml(
   const finalHtmlContent = html.replace(headContentRegex, finalHeadContent);
   return finalHtmlContent;
 }
+
+export function injectCssIntoHtml(html : string, css : string) {
+    const headContentRegex = /<head\b[^>]*>([\s\S]*?)<\/head>/i
+    let headContent = html.match(headContentRegex)
+    let finalHeadContent = ''
+    if (headContent) {
+        // console.log(headContent)
+        let content = headContent[1]
+        finalHeadContent = `<head>` + content + `<style>${css}</style>` + `</head>`
+    }
+    else {
+
+    }
+
+    const finalHtmlContent = html.replace(headContentRegex, finalHeadContent)
+    return finalHtmlContent
+}
+
+
