a9b012518f457cb7f14f8f82ee2f9d7af54c61f9
Mohd Hamza <mohdhamza.me@gmail.com>
2025-12-20 13:07:46 +0530

    seo : add sitmap

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,14 @@
 import React from 'react'
 import { cookies } from "next/headers"
 import { redirect } from "next/navigation"
-
+import GetStarted from './get-started/page'
 export default async function Root() {
     const cookie = await cookies()
     const session = cookie.get("session")?.value
-    // console.log(session)
 
     if (session) {
         redirect("/home")
     }
 
-    redirect("/get-started")
+    return <GetStarted />
 }
