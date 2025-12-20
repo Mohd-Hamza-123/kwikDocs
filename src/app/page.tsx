17d5c9167316a3c7c61b74b33f201d373c267a80
Mohd Hamza <mohdhamza.me@gmail.com>
2025-12-20 18:35:20 +0530

    bug - fix - bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,15 @@ import React from 'react'
 import { cookies } from "next/headers"
 import { redirect } from "next/navigation"
 import GetStarted from './get-started/page'
+
 export default async function Root() {
+
     const cookie = await cookies()
     const session = cookie.get("session")?.value
 
-    // if (session) {
-    //     redirect("/home")
-    // }
+    if (session) {
+        redirect("/home")
+    }
 
     return <GetStarted />
 }
